7331cb9fe13326cb57c113a91bc40294d920a9e7
Damian Tabor <zettie008@gmail.com>
2021-06-02 22:42:11 +0100

    Add class docstring

--- project_10_jack_syntax_analyzer/CompilationEngine.py
+++ project_10_jack_syntax_analyzer/CompilationEngine.py
@@ -1,3 +1,14 @@
 class CompilationEngine:
+    """
+    Class that effects the actual compilation output.
+    Gets its input from a JackTokenizer and emits its
+    parsed structure into an output file/stream.
+
+    This version of the compiler emits a structured
+    printout of the code, wrapped in XML tags. In the final
+    version of the compiler, (project 11), this module
+    generates executable VM code.
+    """
+
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
