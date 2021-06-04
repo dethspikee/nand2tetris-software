ca2ab65ff07e6a462263e79b8bcd38d4081ad062
Damian Tabor <zettie008@gmail.com>
2021-06-04 23:21:38 +0100

    Compile simple while statement

--- project_10_jack_syntax_analyzer/CompilationEngine.py
+++ project_10_jack_syntax_analyzer/CompilationEngine.py
@@ -15,6 +15,82 @@ class CompilationEngine:
 
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
+        self.indent = 0
+
+    def parse(self):
+        while self.tokenizer.has_tokens():
+            if self.tokenizer.token == "while":
+                self.compile_while()
+
+    def compile_statements(self):
+        self.file_obj.write(" " * self.indent + "<statements>\n")
+        if self.tokenizer.token == "let":
+            self.compile_let()
+        self.file_obj.write(" " * self.indent + "</statements>\n")
+
+    def compile_let(self):
+        self.indent += 2
+        self.file_obj.write(" " * self.indent + f"<letStatement>\n")
+        self.indent += 2
+        self._eat("let")
+        self._eat(self.tokenizer.token)
+        self._eat("=")
+        self.compile_expression()
+        self._eat(";")
+        self.indent -= 2
+        self.file_obj.write(" " * self.indent + f"<letStatement>\n")
+        self.indent -= 2
+
+    def compile_while(self):
+        self.file_obj.write("<whileStatement>\n")
+        self.indent += 2
+        self._eat("while")
+        self._eat("(")
+        self.compile_expression()
+        self._eat(")")
+        self._eat("{")
+        self.compile_statements()
+        self._eat("}")
+        self.indent -= 2
+        self.file_obj.write("</whileStatement>\n")
+
+    def compile_expression(self):
+        self.file_obj.write(" " * self.indent + "<expression>\n")
+        self.indent += 2
+        self.complile_term()
+        op = self.tokenizer.token
+        if self.tokenizer.token_type() == "SYMBOL":
+            self._eat(op)
+            self.complile_term()
+        self.indent -= 2
+        self.file_obj.write(" " * self.indent + "</expression>\n")
+
+    def complile_term(self):
+        self.file_obj.write(" " * self.indent + "<term>\n")
+        self.indent += 2
+        varname = self.tokenizer.token
+        next_token = next(self.tokenizer.tokens)
+        if next_token == ".":
+            pass
+        elif next_token == "(":
+            pass
+        elif next_token == "[":
+            pass
+        else:
+            classification = self.tokenizer.get_token_classification()
+            self.file_obj.write(" " * self.indent + f"<{classification}>")
+            self.file_obj.write(f" {self.tokenizer.token} ")
+            self.file_obj.write(f"</{classification}>\n")
+        self.indent -= 2
+        self.file_obj.write(" " * self.indent + "</term>\n")
+        self.tokenizer.token = next_token
+
+    def _eat(self, token: str):
+        classification = self.tokenizer.get_token_classification()
+        self.file_obj.write(" " * self.indent + f"<{classification}>")
+        self.file_obj.write(f" {token} ")
+        self.file_obj.write(f"</{classification}>\n")
+        self.tokenizer.advance()
 
     def show_tokens(self):
         print(list(self.tokenizer.tokens))
