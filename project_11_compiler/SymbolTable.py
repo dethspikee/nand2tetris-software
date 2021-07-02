bee0f952b0e8d9978616d6a45b7d5985875d55da
Damian Tabor <zettie008@gmail.com>
2021-07-02 13:47:21 +0100

    Create basic API

---
+++ project_11_compiler/SymbolTable.py
@@ -0,0 +1,58 @@
+from typing import Union
+
+
+class SymbolTable:
+    def __init__(self):
+        """
+        Creates a new empty symbol table.
+        """
+        pass
+    
+    def start_subroutine(self) -> None:
+        """
+        Starts a new subroutine scope
+        (i.e., resets the subroutine's
+        symbol table).
+        """
+        pass
+
+    def define(name: str, type: str, kind: str) -> None:
+        """
+        Defines a new identifier of a given name,
+        type, and kind and assigns it a running index.
+        STATIC and FIELD identifiers have a class scope,
+        while ARG and VAR identifiers have a subroutine
+        scope.
+        """
+        pass
+
+    def var_count(kind: str) -> int:
+        """
+        Returns the number of variables of the
+        given kind already defined in the current
+        scope.
+        """
+        pass
+
+    def kind_of(name: str) -> Union[str, None]:
+        """
+        Returns the kind of the named
+        identifier in the current scope.
+        If the identifier is unknown in
+        the current scope, returns None.
+        """
+        pass
+
+    def type_of(name: str) -> str:
+        """
+        Returns the type of the named
+        identifier in the current scope.
+        """
+        pass
+
+    def index_of(name: str) -> int:
+        """
+        Returns the index assigned to the 
+        named identifier.
+        """
+        pass
