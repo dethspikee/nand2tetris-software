b69152477c58555a872a8f78d8b89521114cf2e7
Damian Tabor <zettie008@gmail.com>
2021-07-05 21:41:19 +0100

    Use JackCompiler.py as entrypoint

--- project_11_compiler/JackCompiler.py
+++ project_11_compiler/JackCompiler.py
@@ -1,8 +1,25 @@
-import os
 import sys
+import os
+
+from JackTokenizer import JackTokenizer
+from CompilationEngine import CompilationEngine
 
 
-def main():
+def main() -> None:
+    """
+    Entrypoint of the syntax analyzer, expects input path.
+    Uses two classes internally:
+
+    JackTokenizer     :: responsible for retrieving all tokens
+                         from the input file | input dir.
+
+    CompilationEngine :: responsible for parsing tokes and building
+                         grammatical structure represented via the XML
+                         file(s).
+                         Each xxx.jack file will produce xxx.xml file.
+                         Directory with x number of jack files will produce
+                         x number of xml files stored in the root directory.
+    """
     if len(sys.argv) != 2:
         print("Illegal number of arguments. (1 required)")
         print("Path to .jack file or dir with .jack files required.")
@@ -10,14 +27,22 @@ def main():
 
     program_arg = sys.argv[1]
     if os.path.isdir(program_arg):
-        print("Directory!")
-        sys.exit(0)
+        jack_files = [
+            os.path.join(program_arg, name)
+            for name in os.listdir(program_arg)
+            if name.endswith(".jack")
+            and os.path.isfile(os.path.join(program_arg, name))
+        ]
+        for file_ in jack_files:
+            tokenizer = JackTokenizer(file_)
+            with CompilationEngine(tokenizer) as compiler:
+                compiler.parse()
+            tokenizer.file_obj.close()
     elif program_arg.endswith(".jack"):
-        print("File!")
-        sys.exit(0)
-    else:
-        print("Couldn't find .jack file(s)")
-        sys.exit(1)
+        tokenizer = JackTokenizer(program_arg)
+        with CompilationEngine(tokenizer) as compiler:
+            compiler.parse()
+        tokenizer.file_obj.close()
 
 
 if __name__ == "__main__":
